e7ffad47ba6979cc2442e7d47688072a5f5a6de9
Alfredo Paez <alfredo123.apne@gmail.com>
2021-06-28 10:28:16 -0500

    First proyect

---
+++ platzigram/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from platzigram import views as local_views
+from posts import views as posts_views
+from users import views as users_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('hello-world/', local_views.hello_world ),
+    path('sorted/', local_views.hi),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+
+    path('posts/', posts_views.list_post),
+
+    path('users/login', users_views.login_view),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
